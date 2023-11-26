59603b97d4624eb5950a898dc9147fb40a8bc35c
Lukas Weber <l.alex.w@web.de>
2023-11-26 22:22:04 +0100

    upgrade project to wagtail 5.2.1

--- flavours/migrations/0002_flavoursuggestionpage.py
+++ flavours/migrations/0002_flavoursuggestionpage.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import django.db.models.deletion
-import wagtail.wagtailcore.fields
+import wagtail.fields
 
 
 class Migration(migrations.Migration):
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
             name='FlavourSuggestionPage',
             fields=[
                 ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
-                ('intro', wagtail.wagtailcore.fields.RichTextField(blank=True)),
+                ('intro', wagtail.fields.RichTextField(blank=True)),
                 ('thankyou_page_title', models.CharField(help_text="Text to display as the title of the 'thank you' page", max_length=255)),
             ],
             options={
--- flavours/models.py
+++ flavours/models.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 from django.shortcuts import render
 
-from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class IceCreamFlavour(models.Model):
