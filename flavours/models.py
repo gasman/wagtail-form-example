f89a12d679d9bdae061b1766deb1c9ec57fabf53
Lukas Weber <l.alex.w@web.de>
2023-12-27 11:13:13 +0100

    register IceCreamFlavour as snippet

--- flavours/models.py
+++ flavours/models.py
@@ -6,8 +6,10 @@ from django.shortcuts import render
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from wagtail.snippets.models import register_snippet
 
 
+@register_snippet
 class IceCreamFlavour(models.Model):
     flavour_name = models.CharField(max_length=255)
     your_name = models.CharField(max_length=255)
